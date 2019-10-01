9b674700b1819164cbf10c7f485a9a752644a80b
zzy <zzy0310@live.cn>
2019-10-02 07:59:31 +0800

    add odometry

--- src/odometry/src/odometry.cpp
+++ src/odometry/src/odometry.cpp
@@ -14,19 +14,61 @@
 #define depth_min 0.5
 
 using namespace sensor_msgs;
-//using namespace encoder;  //message defined by customer must claim the namespace when used?
+using namespace geometry_msgs;
+using namespace encoder;  //message defined by customer must claim the namespace when used?
 using namespace message_filters;
+
+//PoseStamped message: 
+// std_msgs/Header header
+//   uint32 seq
+//   time stamp
+//   string frame_id
+// geometry_msgs/Pose pose
+//   geometry_msgs/Point position
+//     float64 x
+//     float64 y
+//     float64 z
+//   geometry_msgs/Quaternion orientation
+//     float64 x
+//     float64 y
+//     float64 z
+//     float64 w
+
+//define messages//
+encoder::encoder pre_encoder_msg;
+Image pre_color_im, pre_depth_im;
+PoseStamped pre_pose_msg, cur_pose_msg;
+//odometry semaphore and flag
+bool semaphore = true, first_time = true;
 
 void callback(const ImageConstPtr& color_image, const ImageConstPtr& depth_image, const encoder::encoderConstPtr& encoder)
 {
   // Solve all of perception here...
+  pre_pose_msg.pose.position.x = 0;
 }
 
 int main(int argc, char **argv)
 {
+  //deifne node
   ros::init(argc, argv, "odometry");
   ros::NodeHandle nh;
 
+  //define publisher
+  ros::Publisher pub = nh.advertise<geometry_msgs::PoseStamped>("pose", 50);
+  //define the publish rate 
+  ros::Rate loop_rate(30.0);
+
+  //initial Pose message
+  pre_pose_msg.header.stamp = ros::Time::now();
+  pre_pose_msg.pose.position.x = 0;
+  pre_pose_msg.pose.position.y = 0;
+  pre_pose_msg.pose.position.z = 0;
+  pre_pose_msg.pose.orientation.w = 1;
+  pre_pose_msg.pose.orientation.x = 0;
+  pre_pose_msg.pose.orientation.y = 0;
+  pre_pose_msg.pose.orientation.z = 0;
+
+  //define subscriber using message filter
   message_filters::Subscriber<Image> camera_color_sub(nh, "camera_color", 50);
   message_filters::Subscriber<Image> camera_depth_sub(nh, "camera_depth", 50);
   message_filters::Subscriber<encoder::encoder> encoder_sub(nh, "encoder", 100);
@@ -37,6 +79,18 @@ int main(int argc, char **argv)
   //TimeSynchronizer<Image, Image, encoder::encoder> sync(camera_color_sub, camera_depth_sub, encoder_sub, 50);
   sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
-  ros::MultiThreadedSpinner(); 
+  ros::AsyncSpinner spinner(4);
+  spinner.start();
+
+  //publish the pose message
+  while (ros::ok())
+  {
+    //...
+    ROS_INFO("I am here.");
+
+    //sleep to match the publish rate
+    loop_rate.sleep();
+  }
+
   return 0;
 }
